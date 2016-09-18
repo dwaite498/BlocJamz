3d056a6f0a71608066a70349247429b2196aa38a
Davey <dwaite498@gmail.com>
2016-09-18 15:52:28 -0400

    debug code to find the rest of syntax errors

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function() {
+    var points = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
      var revealPoint = function(index) {
          points[index].style.opacity = 1;
          points[index].style.transform = "scaleX(1) translateY(0)";
@@ -9,19 +9,6 @@ var animatePoints = function(points) {
      }
 
      for (var i = 0; i < points.length; i++) {
-        revealpoint(i);
+        revealPoint(i);
      }
  };
-
-window.onload = function() {
-    if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
-     }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-        animatePoints(pointsArray);
-        }
-    });
-}
