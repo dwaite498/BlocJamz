dbb13f1e505ae9dfd355cb24b7467f5251603b86
Davey <dwaite498@gmail.com>
2016-09-18 20:36:21 -0400

    debugging collection.js

--- scripts/collection.js
+++ scripts/collection.js
@@ -14,11 +14,13 @@
    + '</div>'
    ;
 
-window.onload = function() {
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    collectionContainer.innerHTML = '';
-    
-    for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
-    }
-}
+ window.onload = function() {
+
+     var collectionContainer = document.getElementsByClassName('album-covers')[0];
+  
+     collectionContainer.innerHTML = '';
+ 
+     for (var i = 0; i < 12; i++) {
+         collectionContainer.innerHTML += collectionItemTemplate;
+     }
+ }
