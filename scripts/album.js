6ed6edae42ae97f27b7d0d00f99792db82aae47f
Davey <dwaite498@gmail.com>
2016-09-23 17:22:06 -0400

    bugs

--- scripts/album.js
+++ scripts/album.js
@@ -56,17 +56,18 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 }
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
  var setCurrentAlbum = function(album) {
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
+     albumImage.setAttribute('src', album.albumArtURL);
      albumSongList.innerHTML = '';
  
      for (var i = 0; i < album.songs.length; i++) {
@@ -79,7 +80,7 @@ var createSongRow = function(songNumber, songName, songLength) {
      
      var albums = [albumPicasso, albumMarconi, albumWellington];
      var index = 1;
-     album-cover-art.addEventListener("click", function(event) {
+     albumImage.addEventListener("click", function(event) {
         setCurrentAlbum(album[index]);
         index++
         if(index == albums.length) {
