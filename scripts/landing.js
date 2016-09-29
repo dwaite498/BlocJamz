76744bac8aa835cde3b0d19816f8aae7206d401f
Davey <dwaite498@gmail.com>
2016-09-29 17:18:58 -0400

    jquery code add

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,25 +1,20 @@
-var pointsArray = document.getElementsByClassName('point');
-
-      var revealPoint = function(point) {
-         point.style.opacity = 1;
-         point.style.transform = "scaleX(1) translateY(0)";
-         point.style.msTransform = "scaleX(1) translateY(0)";
-         point.style.WebkitTransform = "scaleX(1) translateY(0)";
-
-     }
- var animatePoints = function(points) {
-        forEach(points, revealPoint);
+ var animatePoints = function() {
+     var revealPoint = function() {
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+              $.each($('.point'), revealPoint);
  };
-
-window.onload = function() {
-    if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
+$(window).load(function {
+     if ($(window).height() > 950) {
+         animatePoints();
      }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-        animatePoints(pointsArray);
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+     $(window).scroll(function(event) {
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
         }
     });
-}
+});
