584faa7807a9a884d0afad4e728c625ff6a01871
Davey <dwaite498@gmail.com>
2016-10-04 19:19:59 -0400

    update

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function() {
+    var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -14,14 +15,18 @@ var collectionItemTemplate =
    + '</div>'
    ;
 
- window.onload = function() {
 
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
+    return $(template);
+};
 
-     collectionContainer.innerHTML = '';
+$(window).load(function() {
+    
+    var $collectionContainer = $('album-covers');
+    $collectionContainer.empty();
  
 
      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+         var $newThumbnail = buildCollectionItemTemplate();
+         $collectionContainer.append($newThumbnail);
      }
- }
+});
