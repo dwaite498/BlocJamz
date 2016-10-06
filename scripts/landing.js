7b8e01d32471e1cd05d7b066523e2a0d5b1b4610
Davey <dwaite498@gmail.com>
2016-10-05 20:54:13 -0400

    clean up 17 for new branch

--- scripts/landing.js
+++ scripts/landing.js
@@ -6,12 +6,14 @@
          });
               $.each($('.point'), revealPoint);
     };
+ 
 $(window).load(function() {
      if ($(window).height() > 950) {
          animatePoints();
      }
+    
     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
-     $(window).scroll(function(event) {
+    $(window).scroll(function(event) {
          if ($(window).scrollTop() >= scrollDistance) {
              animatePoints();
         }
