0cd7b939bf94302c8de89394642fd27d4c058b22
Davey <dwaite498@gmail.com>
2016-10-11 21:35:13 -0400

    add fixtures.js update click function

--- scripts/album.js
+++ scripts/album.js
@@ -1,49 +1,3 @@
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtURL: 'assets/images/album_covers/01.png',
-    songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21'},
-        { title: 'Magenta', duration: '2:15'}
-    ]
-};
-
-var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtURL: 'assets/images/album_covers/20.png',
-    songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21' },
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15' }
-     ]
-};
-
-var albumWellington = {
-    title: 'The General',
-    artist: 'Arthur Wellesley',
-    label: 'Great Britain',
-    year: '1815',
-    albumArtURL: 'assets/images/album_covers/20.png',
-    songs: [
-        { title: 'Dublin Born', duration: '2:32' },
-        { title: 'Irish Soldier', duration: '3:01' },
-        { title: 'Peninsular Campaign', duration: '1:45' },
-        { title: 'Ambassador to France', duration: '1:00' },
-        { title: 'Waterloo Campaign', duration: '5:21' },
-        { title: 'Prime Minister Wellesley', duration: '3:22' }
-    ]
-};
-
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
      '<tr class="album-view-song-item">'
@@ -59,17 +13,17 @@ var createSongRow = function(songNumber, songName, songLength) {
          var songNumber = $(this).click('data-song-number');
          debugger;
          
-         if (currentlyPlayingSong !== null) {
-             var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-             currentlyPlayingCell.html(currentlyPlayingSong);
+         if (currentlyPlayingSongNumber !== null) {
+             var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+             currentlyPlayingCell.html(currentlyPlayingSongNumber);
          }
-         if (currentlyPlayingSong !== songNumber) {
+         if (currentlyPlayingSongNumber !== songNumber) {
              $(this).html(pauseButtonTemplate);
-             currentlyPlayingSong = songNumber;
+             currentlyPlayingSongNumber = songNumber;
          }
-         else if (currentlyPlayingSong === songNumber) {
+         else if (currentlyPlayingSongNumber === songNumber) {
              $(this).html(playButtonTemplate);
-             currentlyPlayingSong = null;
+             currentlyPlayingSongNumber = null;
          }
      };
     
@@ -77,7 +31,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         var songNumberCell = $(this).find('.song-item-number');
         var songNumber = songNumberCell.attr('data-song-number');
          
-        if (songNumber !== currentlyPlayingSong) {
+        if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(playButtonTemplate);
         }
      };
@@ -85,7 +39,7 @@ var createSongRow = function(songNumber, songName, songLength) {
          var songNumberCell = $(this).find('.song-item-number');
          var songNumber = songNumberCell.attr('data-song-number');
          
-         if(songNumber !== currentlyPlayingSong) {
+         if(songNumber !== currentlyPlayingSongNumber) {
              songNumberCell.html(songNumber);
          }
          
@@ -106,6 +60,7 @@ var albumImage = document.getElementsByClassName('album-cover-art')[0];
 var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
  var setCurrentAlbum = function(album) {
+     currentAlbum = album;
      var $albumTitle = $('.album-view-title');
      var $albumArtist = $('.album-view-artist');
      var $albumReleaseInfo = $('.album-view-release-info');
@@ -127,7 +82,11 @@ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></
 
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
-var currentlyPlayingSong = null;
+var currentlyPlayingSongNumber = null;
+
+var currentAlbum = null;
+
+var currentSongFromAlbum = null;
 
 
 
---
+++ scripts/fixtures.js
@@ -0,0 +1,45 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtURL: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtURL: 'assets/images/album_covers/20.png',
+    songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+};
+
+var albumWellington = {
+    title: 'The General',
+    artist: 'Arthur Wellesley',
+    label: 'Great Britain',
+    year: '1815',
+    albumArtURL: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Dublin Born', duration: '2:32' },
+        { title: 'Irish Soldier', duration: '3:01' },
+        { title: 'Peninsular Campaign', duration: '1:45' },
+        { title: 'Ambassador to France', duration: '1:00' },
+        { title: 'Waterloo Campaign', duration: '5:21' },
+        { title: 'Prime Minister Wellesley', duration: '3:22' }
+    ]
+};
