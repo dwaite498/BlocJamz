7a5bfa9e9f0ef6020259e70d12786041cacb45f4
Davey <dwaite498@gmail.com>
2016-10-11 21:13:28 -0400

    replace bugged function with fresh code
    it works now.

--- scripts/album.js
+++ scripts/album.js
@@ -56,22 +56,23 @@ var createSongRow = function(songNumber, songName, songLength) {
      var $row = $(template);
     
      var clickHandler = function() {
-         var songNumber = $(this).click('data-song-number');
-         debugger;
-         
-         if (currentlyPlayingSong !== null) {
-             var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-             currentlyPlayingCell.html(currentlyPlayingSong);
-         }
-         if (currentlyPlayingSong !== songNumber) {
-             $(this).html(pauseButtonTemplate);
-             currentlyPlayingSong = songNumber;
-         }
-         else if (currentlyPlayingSong === songNumber) {
-             $(this).html(playButtonTemplate);
-             currentlyPlayingSong = null;
-         }
-     };
+	var songNumber = $(this).attr('data-song-number');
+
+	if (currentlyPlayingSong !== null) {
+		// Revert to song number for currently playing song because user started playing new song.
+		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
+		currentlyPlayingCell.html(currentlyPlayingSong);
+	}
+	if (currentlyPlayingSong !== songNumber) {
+		// Switch from Play -> Pause button to indicate new song is playing.
+		$(this).html(pauseButtonTemplate);
+		currentlyPlayingSong = songNumber;
+	} else if (currentlyPlayingSong === songNumber) {
+		// Switch from Pause -> Play button to pause currently playing song.
+		$(this).html(playButtonTemplate);
+		currentlyPlayingSong = null;
+	}
+};
     
      var onHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
